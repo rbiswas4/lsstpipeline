6bb13998456f38a549d3b1ac7b38d0ae491ee461
R. Biswas <rbiswas4@gmail.com>
2015-02-03 07:55:18 -0800

    sncosmo_sim.py with restrict=None to allow all SN to be realized

--- sncosmo_sim.py
+++ sncosmo_sim.py
@@ -9,7 +9,7 @@ import simulate_lsst as sl
 
 # Read a simlib file and create a set of observation tables 
 #restrict to 10 SN from each field
-lcs = sl.simulate_simlib(simlibfile='cache/snana_test.simlib', snmodelsource='salt2-extended', outfile='LC/lc', restrict=10)
+lcs = sl.simulate_simlib(simlibfile='cache/snana_test.simlib', snmodelsource='salt2-extended', outfile='LC/lc', restrict=None)
 # simulate SN
 
 
